232c36d7e544c7800d3363ad846f6b1586f68c37
Alejandro <alesantin012@hotmail.com>
2021-03-21 16:12:43 -0300

    http_utils

--- controllers/items_controller.go
+++ controllers/items_controller.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/Kungfucoding23/bookstore_items_api/domain/items"
 	"github.com/Kungfucoding23/bookstore_items_api/services"
+	"github.com/Kungfucoding23/bookstore_items_api/utils/http_utils"
 	"github.com/Kungfucoding23/bookstore_oauth-go/oauth"
+	"github.com/Kungfucoding23/bookstore_utils-go/rest_errors"
 )
 
 var (
@@ -22,17 +25,33 @@ type itemsController struct{}
 
 func (c *itemsController) Create(w http.ResponseWriter, r *http.Request) {
 	if err := oauth.AuthenticateRequest(r); err != nil {
-		//TODO: return error to the caller
+		restErr := rest_errors.NewBadRequestError("invalid request body")
+		http_utils.RespondError(w, restErr)
+		return
 	}
-	item := items.Item{
-		Seller: oauth.GetCallerID(r),
-	}
-	result, err := services.ItemsService.Create(item)
+
+	var itemRequest items.Item
+
+	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		//TODO: return error json to the user
+		respErr := rest_errors.NewBadRequestError("invalid request body")
+		http_utils.RespondError(w, respErr)
+		return
 	}
-	fmt.Println(result)
-	//TODO: return created item as json with HTTP status 201 created.
+	defer r.Body.Close()
+	if err := json.Unmarshal(requestBody, &itemRequest); err != nil {
+		respErr := rest_errors.NewBadRequestError("invalid json body")
+		http_utils.RespondError(w, respErr)
+		return
+	}
+
+	itemRequest.Seller = oauth.GetClientID(r)
+	result, CreateErr := services.ItemsService.Create(itemRequest)
+	if err != nil {
+		http_utils.RespondError(w, CreateErr)
+		return
+	}
+	http_utils.RespondJSON(w, http.StatusCreated, result)
 }
 
 func (c *itemsController) Get(w http.ResponseWriter, r *http.Request) {
